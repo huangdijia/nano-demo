70251fddffcdd91bc02ec7036ed472c82931af1a
huangdijia <huangdijia@gmail.com>
2022-08-21 13:25:24 +0800

    Optimize

--- app.php
+++ app.php
@@ -66,7 +66,11 @@ class ConfigProvider
             ],
             'processes' => [],
             'redis' => [],
-            'server' => [],
+            'server' => [
+                'settings' => [
+                    'daemonize' => (int) env('DAEMONIZE', 0),
+                ],
+            ],
         ];
     }
 }
