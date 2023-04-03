f0f143dfbc9de28a9f92ea6525cbd59231624e12
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-03 08:37:02 +0800

    Adds websocket example

--- app.php
+++ app.php
@@ -7,95 +7,61 @@ declare(strict_types=1);
  * @link     https://github.com/huangdijia/nano-demo
  * @contact  Huangdijia <huangdijia@gmail.com>
  */
-use Hyperf\Contract\StdoutLoggerInterface;
-use Hyperf\Di\ClassLoader;
+use Hyperf\Contract\OnCloseInterface;
+use Hyperf\Contract\OnMessageInterface;
+use Hyperf\Contract\OnOpenInterface;
 use Hyperf\Nano\Factory\AppFactory;
-use Hyperf\Utils\Composer;
-use Psr\Log\LogLevel;
+use Hyperf\Server\Event;
+use Hyperf\Server\Server;
+use Hyperf\WebSocketServer\Constant\Opcode;
 
 define('BASE_PATH', __DIR__);
 
 require_once BASE_PATH . '/vendor/autoload.php';
 
-class ConfigProvider
+$app = AppFactory::createBase('0.0.0.0', 9501);
+
+$app->config(['server.servers.1' => [
+    'name' => 'ws',
+    'type' => Server::SERVER_WEBSOCKET,
+    'host' => '0.0.0.0',
+    'port' => 9502,
+    'sock_type' => SWOOLE_SOCK_TCP,
+    'callbacks' => [
+        Event::ON_HAND_SHAKE => [\Hyperf\WebSocketServer\Server::class, 'onHandShake'],
+        Event::ON_MESSAGE => [\Hyperf\WebSocketServer\Server::class, 'onMessage'],
+        Event::ON_CLOSE => [\Hyperf\WebSocketServer\Server::class, 'onClose'],
+    ],
+],
+]);
+
+class WebSocketController implements OnMessageInterface, OnOpenInterface, OnCloseInterface
 {
-    public function __invoke()
+    public function onMessage($server, $frame): void
     {
-        return [
-            'app_name' => env('APP_NAME', 'nano-demo'),
-            'app_env' => env('APP_ENV', 'dev'),
-            'scan_cacheable' => env('SCAN_CACHEABLE', false),
-            StdoutLoggerInterface::class => [
-                'log_level' => [
-                    LogLevel::ALERT,
-                    LogLevel::CRITICAL,
-                    LogLevel::DEBUG,
-                    LogLevel::EMERGENCY,
-                    LogLevel::ERROR,
-                    LogLevel::INFO,
-                    LogLevel::NOTICE,
-                    LogLevel::WARNING,
-                ],
-            ],
-            'annotations' => [
-                'scan' => [
-                    'paths' => [
-                        // BASE_PATH . '/',
-                    ],
-                    'ignore_annotations' => [
-                        'mixin',
-                    ],
-                    'class_map' => [],
-                ],
-            ],
-            'aspects' => [],
-            'commands' => [],
-            'crontab' => [],
-            'databases' => [],
-            'dependencies' => [],
-            'exceptions' => [
-                'handler' => [
-                    'http' => [
-                        // App\Exception\Handler\BusinessExceptionHandler::class,
-                    ],
-                ],
-            ],
-            'listeners' => [
-                Hyperf\ExceptionHandler\Listener\ErrorExceptionHandler::class,
-                Hyperf\Command\Listener\FailToHandleListener::class,
-            ],
-            'logger' => [],
-            'middlewares' => [
-                'http' => [],
-            ],
-            'processes' => [],
-            'redis' => [],
-            'server' => [
-                'settings' => [
-                    'daemonize' => (int) env('DAEMONIZE', 0),
-                ],
-            ],
-            'signal' => [
-                'handlers' => [
-                    Hyperf\Signal\Handler\WorkerStopHandler::class => PHP_INT_MIN,
-                ],
-                'timeout' => 5.0,
-            ],
-        ];
+        if ($frame->opcode == Opcode::PING) {
+            // 如果使用协程 Server，在判断是 PING 帧后，需要手动处理，返回 PONG 帧。
+            // 异步风格 Server，可以直接通过 Swoole 配置处理，详情请见 https://wiki.swoole.com/#/websocket_server?id=open_websocket_ping_frame
+            $server->push('', Opcode::PONG);
+            return;
+        }
+        $server->push($frame->fd, 'Recv: ' . $frame->data);
+    }
+
+    public function onClose($server, int $fd, int $reactorId): void
+    {
+        var_dump('closed');
+    }
+
+    public function onOpen($server, $request): void
+    {
+        $server->push($request->fd, 'Opened');
     }
 }
 
-Closure::bind(function () {
-    self::getLockContent();
-    self::$extra[uniqid()] = [
-        'hyperf' => [
-            'config' => ConfigProvider::class,
-        ],
-    ];
-    ClassLoader::init();
-}, null, Composer::class)();
-
-$app = AppFactory::createBase('0.0.0.0', 9501);
+$app->addServer('ws', function () use ($app) {
+    $app->get('/', WebSocketController::class);
+});
 
 $app->get('/', function () {
     $user = $this->request->input('user', 'nano');
